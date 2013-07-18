90bc03f149c0f8941ab3e84b247b43a356b97bf6
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-07-18 21:21:37 +0200

    use of express module

--- index.js
+++ index.js
@@ -1,49 +1,64 @@
 /* Modules */
-var colors								= require("colors");
-var prototypejs							= require('prototype'); 
-Object.extend(global, prototypejs);
-var server								= require("./server");
-var router								= require("./router");
-var requestHandlers						= require("./requestHandlers");
+var colors                              = require("colors");
+var prototypejs                         = require('prototype');
+Object.extend(global, prototypejs);     // to facilitate use of prototype
+var http                                = require("http");
+var url                                 = require("url");
+var fs                                  = require("fs");
+var express                             = require("express");
+app                                     = express();
+var server                              = http.createServer(app);
+var io                                  = require('socket.io').listen(server);
+/* Configuration */
+var port                                = process.env.PORT || 80;
 
-/* Variables */
-var handle								= {};
+  /***********/
+ /* Serveur */
+/***********/
+server.listen( port );
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+console.log( "      Server listening on port : ".cyan.bold + port );
 
-/************
-*************
-**
-** PAGES HTML
-**
-*************
-************/
-handle["/"]								= requestHandlers.index;
-handle["/index"]						= requestHandlers.index;
-handle["/index.html"]					= requestHandlers.index;
 
-/**************
-***************
-**
-** FICHIERS CSS
-**
-***************
-**************/
-handle["/css/normalize.css"]			= requestHandlers.normalize;
-handle["/css/grid.css"]					= requestHandlers.grid;
-handle["/css/animations.css"]			= requestHandlers.animations;
-handle["/css/styles.css"]				= requestHandlers.styles;
+// Here we play with sockets
 
-/*********************
-**********************
+/***********
 **
-** FICHIERS JAVASCRIPT
+** Socket.IO and the game
 **
-**********************
-*********************/
-handle["/js/jquery-1.9.1.js"]			= requestHandlers.jquery;
-handle["/js/mustache.js"]				= requestHandlers.mustache;
-handle["/js/client.js"]					= requestHandlers.client;
-handle["/js/prototype-1.7.1.js"]		= requestHandlers.prototype;
-handle["/js/Classes/class_case.js"]		= requestHandlers.classCase;
-handle["/js/morpion_client.js"]			= requestHandlers.morpionClient;
+***********/
+var players = 0;
 
-server.index(router.route, handle);
+io.sockets.on('connection', function(socket){
+    players++;
+    var me = "player"+players;
+    socket.emit("youArePlayer", me)
+
+    if (me === "player2") {
+        io.sockets.emit("startGame");
+    }
+
+    var contenu = {};
+    // var dataPre;
+    socket.on('endTurn', function(data){
+         // if (dataPre) {
+         //   if (!chechData(dataPre,data)) {
+         //    // vérifier joueur du tour
+         //    // controle des cases existante
+         //    // vérifier qu'il n'y a pas plus d'une case de joué
+         //    return "triche !";
+         //   }
+         // }
+         // dataPre = data;
+         io.sockets.emit('changeTurn', data);
+    });
+
+    socket.on('disconnect', function(){
+        players--;
+    });
+});
