508991b93e8448773f9590a00cff63c6431b4742
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-07-25 16:46:48 +0200

    added views management

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,12 +1,21 @@
-﻿localStorage.removeItem('id_carre');
-var id_existe = false;
-
-function Ctrl($scope) {
-    $scope.templates = [
-	    {url: './partials/menu.html'},
-	    {url: './partials/game.html'}
-    ];
-    
-	$scope.templateMenu = $scope.templates[0];
-	$scope.templateGame = $scope.templates[1];
+﻿function Ctrl($scope) {    
+    $scope.templateMenu    = './partials/menu.html';
+    $scope.templateGame    = './partials/game.html';
+    $scope.templateOptions = './partials/options.html';
+    $scope.templateRules   = './partials/rules.html';
 }
+
+jQuery(function ($){
+    console.log("interface.js ok");
+    
+    $('body').on('click', '.changeViewButton', function(event){
+        var target = "view_" + $(this).attr("data-viewTarget");
+        $(".activeView").removeClass("activeView").fadeOut(300, function(){
+            $("#" + target).addClass("activeView").fadeIn(300);
+        });
+    });
+
+    $('body').on('click', '.quitButton', function(event){
+        window.close();
+    });
+});
