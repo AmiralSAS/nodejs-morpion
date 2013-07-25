16db4344fa5a4b783dd57267956858486f3cf598
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-07-25 02:44:54 +0200

    added angularjs and include html elements into index.html

---
+++ README.md
@@ -0,0 +1,6 @@
+NodeJS Morpion
+==============
+
+Version 0.0.2 (B)
+
+Morpion / TicTacToe multiplayer game in developpement.
---
+++ public/js/interface.js
@@ -0,0 +1,12 @@
+﻿localStorage.removeItem('id_carre');
+var id_existe = false;
+
+function Ctrl($scope) {
+    $scope.templates = [
+	    {url: './partials/menu.html'},
+	    {url: './partials/game.html'}
+    ];
+    
+	$scope.templateMenu = $scope.templates[0];
+	$scope.templateGame = $scope.templates[1];
+}
